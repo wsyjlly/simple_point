d399d40900871eb6026f7daf5807f72ca5555c32
简单点 <35170789+wsyjlly@users.noreply.github.com>
2018-10-24 08:59:11 +0800

    Update AddressLocation.java

--- operating_system/src/cn/wsyjlly/addresspage/AddressLocation.java
+++ operating_system/src/cn/wsyjlly/addresspage/AddressLocation.java
@@ -15,33 +15,25 @@ public class AddressLocation {
 	private final static int pagesize = 1024;
 	private final static int page = 64;
 	private final static int[] pagetable = {0,42,29,15,45,31,44,43, 
-									41,28,1,30,12,24,6,32, 
-									14,27,13,46,7,33,10,22, 
-									40,2,51,11,39,23,49,50, 
-									26,16,25,4,47,17,3,48, 
-									52,36,58,35,57,34,21,63, 
-									5,37,18,8,62,56,20,54, 
-									60,19,38,9,61,55,59,53};
+						41,28,1,30,12,24,6,32, 
+						14,27,13,46,7,33,10,22, 
+						40,2,51,11,39,23,49,50, 
+						26,16,25,4,47,17,3,48, 
+						52,36,58,35,57,34,21,63, 
+						5,37,18,8,62,56,20,54, 
+						60,19,38,9,61,55,59,53};
 	
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-//		Set<Integer> list = new HashSet<Integer>();
-//		for(int i = 0; i<64;i++){
-//			list.add(i);
-//		}
-//		Iterator<Integer> iterator = list.iterator();
-//
-//		System.out.println(iterator.next());
-//		System.out.println(iterator.next());
-//		System.out.println(iterator.next());
-//		System.out.println(iterator.next());
-//		if(iterator.hasNext()){
-//			Integer next = iterator.next();
-//			System.out.println(next);
-//		}
-		
+		System.out.println("系统页号对应块号情况（页号——>块号）：");
+		System.out.printf("--------------------------------------------------------------------------------");
+		for (int i = 0; i < pagetable.length; i++) {
+			if(i%10==0)System.out.println();
+			System.out.printf(i+"——>"+pagetable[i]+"\t");
+		}
+		System.out.printf("\n--------------------------------------------------------------------------------");
 		while(true){
-			System.out.println("请输入逻辑地址(请输入9位及以下数字)：");
+			System.out.println("\n请输入逻辑地址(请输入9位及以下数字)：");
 			int address = new Scanner(System.in).nextInt();
 			
 			int pagenum = address/pagesize;
@@ -54,10 +46,6 @@ public class AddressLocation {
 				System.out.println("对应的物理地址页地址为："+deviation);
 				System.out.println("对应的物理地址为："+add);
 			}
-			
 		}
-		
-
 	}
-
 }
